67f8fed08ecfdbcc33d10d9b9d129d21fbc866d9
Max Countryman <max.countryman@gmail.com>
2013-04-06 12:22:24 -0700

    using marginalia

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-core "1.1.6"]
-                 [ring/ring-jetty-adapter "1.1.6"]])
+                 [ring/ring-jetty-adapter "1.1.6"]]
+  :plugins [[lein-marginalia "0.7.1"]])
