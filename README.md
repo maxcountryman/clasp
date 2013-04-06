a7e6b26ca81a83cb63cd76749b2a81e39e049007
Max Countryman <max.countryman@gmail.com>
2013-04-06 11:24:19 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Clasp
+
+A dead simple routing DSL for Clojure's ring.
+
+## Usage
+
+...
+
+## License
+
+Copyright © 2013 Max Countryman
+
+Distributed under the BSD License.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clasp "0.0.1"
+  :description "A dead simple routing DSL for Clojure's ring."
+  :url "https://github.com/maxcountryman/clasp"
+  :license {:name "BSD"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-core "1.1.6"]
+                 [ring/ring-jetty-adapter "1.1.6"]])
